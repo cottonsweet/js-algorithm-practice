ed0719cf1934decc0cdea81d2954867632fbadf2
lk1 <whitecandy777@naver.com>
2025-11-09 23:01:30 +0900

    feat: 대문자로 통일

---
+++ 1.기본문제 풀이/대문자로 통일/answer.js
@@ -0,0 +1,11 @@
+function solution(str) {
+  let result = "";
+
+  result = str.toUpperCase();
+  console.log(result);
+
+  return result;
+}
+
+const strVariable = "KoreaTimeGood";
+solution(strVariable);
